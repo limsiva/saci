d27d1a10330d194da5d0fe7764f888e53fd94e4b
limsiva <38914974+limsiva@users.noreply.github.com>
2023-10-25 19:14:35 -0300

    update encontros

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var encontros = [
     {"title": "1. Apresentação Geral da Disciplina - 24/08/2023", "uri": "https://limsiva.github.io/saci/encontros/dia_1.html"},
     {"title": "2. Aula Inaugural: Direitos, Saúde, Cidadania e Diversidade - 31/08/2023", "uri": "https://limsiva.github.io/saci/encontros/dia_2.html"},
     {"title": "3. Primeiro dia na Unidade - 14/09/2023", "uri": "https://limsiva.github.io/saci/encontros/dia_3.html"},
+    {"title": "4. Trabalho em Equipe e Técnicas Etnográficas - 21/09/2023", "uri": "https://limsiva.github.io/saci/encontros/dia_4.html"},
+    {"title": "5. IV Seminário de Saúde Coletiva - 28/09/2023", "uri": "https://limsiva.github.io/saci/encontros/dia_5.html"},
     {"title": "6. Passeio Exploratório - 05/10/2023", "uri": "https://limsiva.github.io/saci/encontros/dia_6.html"},
 ]
 
