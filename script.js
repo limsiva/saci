09cf12d1253f4b53291287ec63da915c977fb4ce
limsiva <38914974+limsiva@users.noreply.github.com>
2023-12-08 19:51:22 -0300

    inicio do portfolio final

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ var encontros = [
     {"title": "5. IV Seminário de Saúde Coletiva - 28/09/2023", "uri": "https://limsiva.github.io/saci/encontros/dia_5.html"},
     {"title": "6. Passeio Exploratório - 05/10/2023", "uri": "https://limsiva.github.io/saci/encontros/dia_6.html"},
     {"title": "7. Mapa do Território - 19/10/2023", "uri": "https://limsiva.github.io/saci/encontros/dia_7.html"},
+    {"title": "8. Saúde e seus determinantes, e desigualdades no contexto da saúde da população - 26/10/2023", "uri": "https://limsiva.github.io/saci/encontros/dia_8.html"},
+    {"title": "9. Conselho Comunitário do Soledade I - 09/11/2023", "uri": "https://limsiva.github.io/saci/encontros/dia_9.html"},
+    {"title": "10. -", "uri": "https://limsiva.github.io/saci/encontros/dia_10.html"},
+    {"title": "11. -", "uri": "https://limsiva.github.io/saci/encontros/dia_11.html"},
+    {"title": "12. -", "uri": "https://limsiva.github.io/saci/encontros/dia_12.html"},
+    {"title": "13. -", "uri": "https://limsiva.github.io/saci/encontros/dia_13.html"},
 ]
 
 for (i = 0; i<= encontros.length; i++) {
